1074d920f58232b3b6af23d5c5da6ad4db999193
Pere Urbon-Bayes <pere.urbon@elasticsearch.com>
2015-08-28 13:11:35 +0200

    Add basic test to this plugin, also add a local tag to run this test explicitly, this is because the way the plugin is implemented it need to have a libzmq installed.

--- spec/inputs/zeromq_spec.rb
+++ spec/inputs/zeromq_spec.rb
@@ -1,1 +1,19 @@
-require "logstash/devutils/rspec/spec_helper"
+# encoding: utf-8
+require_relative "../spec_helper"
+require "logstash/plugin"
+require "logstash/event"
+
+describe LogStash::Inputs::ZeroMQ, :zeromq => true do
+
+  context "when register and teardown" do
+
+    let(:plugin) { LogStash::Plugin.lookup("input", "zeromq").new({ "topology" => "pushpull" }) }
+
+    it "should register and teardown without errors" do
+      expect { plugin.register }.to_not raise_error
+      expect { plugin.teardown }.to_not raise_error
+    end
+
+  end
+
+end
